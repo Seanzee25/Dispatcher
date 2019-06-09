baf11197dab592a68d22c9fde704554800a791eb
Sean <seanzee25@gmail.com>
2019-06-09 13:01:25 -0500

    Add user and role model/repository

---
+++ src/main/java/org/launchcode/dispatcher/models/User.java
@@ -0,0 +1,65 @@
+package org.launchcode.dispatcher.models;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String username;
+
+    private String password;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"
+            )
+    )
+    private Collection<Role> roles;
+
+    // TODO Map company
+//    private Business business;
+
+    public User() {}
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+//    public Business getBusiness() {
+//        return business;
+//    }
+//
+//    public void setBusiness(Business business) {
+//        this.business = business;
+//    }
+}
---
+++ src/main/java/org/launchcode/dispatcher/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.launchcode.dispatcher.repositories;
+
+import org.launchcode.dispatcher.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
