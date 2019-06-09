17801818b64924477322ae17e073626e9fcec23e
Sean <seanzee25@gmail.com>
2019-06-09 12:39:17 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/dispatcher/DispatcherApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatcherApplication.class, args);
+	}
+
+}
